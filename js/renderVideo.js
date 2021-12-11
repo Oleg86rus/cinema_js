a0d8848f851711d3e884d284387acea4f064a02a
oleg086rus <o.pototskiy@yandex.ru>
2021-12-11 11:52:03 +0500

    day3_start

--- js/renderVideo.js
+++ js/renderVideo.js
@@ -1,8 +1,9 @@
 import { getTriends } from './servises.js';
+import renderCard from './renderCard.js';
 
 const filmWeek = document.querySelector('.film-week');
 const firstRender = data => {
-    console.log('data: ', data);
+    //console.log('data: ', data);
     filmWeek.innerHTML = `
     <div class="container film-week__container" data-rating="${data.vote_average}">
         <div class="film-week__poster-wrapper">
@@ -17,7 +18,12 @@ const firstRender = data => {
 
 const renderVideo = async () => {
     const data = await getTriends();
-    firstRender(data.results[0]);
+    
+    const [firstCard, ...otherCard] = data.results;
+    otherCard.length = 16;
+   // console.log('otherCard: ', otherCard);
+    firstRender(firstCard);
+    renderCard(otherCard);
 };
 
 export default renderVideo;
